7ca88e65953db4764537e849e47a72856c227285
Samuel <samnmbuguah@gmail.com>
2023-06-23 05:38:28 +0300

    Completed Walkthrough

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,10 +8,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
-
+  
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -22,4 +22,10 @@ class BirdsController < ApplicationController
     end
   end
 
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
